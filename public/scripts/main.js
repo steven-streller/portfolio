f8405b24ef0d1a9837ad7f872e2ee859ee82f56e
Steven Streller <steven@streller.net>
2022-01-02 15:40:52 +0100

    Lebenslauf finish

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -4,4 +4,12 @@ AOS.init({
   duration: 1000
 });
 
-// Add your javascript here
+document.getElementById("birth").innerHTML = (() => {
+  let ageDifMs = Date.now() - new Date("1997-07-23");
+  let ageDate = new Date(ageDifMs);
+  return "23.07.1997 (" + Math.abs(ageDate.getUTCFullYear() - 1970) + ")";
+}).call(this);
+
+document.getElementById("copyright").innerHTML = (() => {
+  return "&copy; " + new Date().getFullYear() + " Alle Rechte vorbehalten.";
+}).call(this);
